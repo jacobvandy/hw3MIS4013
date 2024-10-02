4d2926822607caaacfcd3d7e5f0059bbcfb63690
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-02 12:51:59 -0500

    Update resturant.php

--- resturant.php
+++ resturant.php
@@ -4,7 +4,7 @@ require_once("model-resturant.php");
 
 $pageTitle = "Taco Bell";
 include "view-header.php";
-$resturant = SelectResturant();
+$resturant = SelectRestaurant();
 include "view-resturant.php";
 include "view-footer.php";
 ?>
