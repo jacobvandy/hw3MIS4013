1c28f6c2844da6ffe480302169ac7f4bc6dfa7d3
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-03 16:04:48 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+   }
   return $conn;
 }
 ?>
