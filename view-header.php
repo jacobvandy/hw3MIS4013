9e2b19ff5ef6ca3d27e50ccb920f5f3775687fcc
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-03 16:28:09 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,7 +20,7 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="resturant.php">Resturant</a>
+          <a class="nav-link" href="resturant.php">Restaurant</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="menu.php">Menu</a>
