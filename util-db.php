3d63ea12af9da57fc21f3727e65c10582e68d901
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-03 16:53:21 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -3,7 +3,7 @@ function get_db_connection() {
   $servername = "mis4013-jv.mysql.database.azure.com";
   $username = "jacobvandy";
   $password = "mis4013!";
-  $dbname = "jacobvandy";
+  $dbname = "mis4013-jv";
   $port = 3306;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
