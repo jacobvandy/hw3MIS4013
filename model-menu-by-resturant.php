b33afe038bdad2159d644dcf08a63eb3b6fdaac4
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-03 18:56:00 -0500

    Create model-menu-by-resturant.php

---
+++ model-menu-by-resturant.php
@@ -0,0 +1,16 @@
+<?php
+function SelectMenuBtRestaurant($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT mi.MenuItemID, ItemName, Price, City, State, Address FROM menuitems mi join locations l on mi.LocationID = l.LocationID WHERE l.RestaurantID = ?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
