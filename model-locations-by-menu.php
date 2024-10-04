3f75581901c07d8f36c2ddbf8762c79d16616029
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-04 12:12:37 -0500

    Update model-locations-by-menu.php

--- model-locations-by-menu.php
+++ model-locations-by-menu.php
@@ -4,7 +4,7 @@ function SelectLocationsByMenu($lid) {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT mi.LocationID, mi.RestaurantID, Price, City, State, Address FROM menuitems mi join locations l
 on mi.LocationID = l.LocationID
-WHERE mi.MenuItemID = ?");
+WHERE MenuItemID = ?");
         $stmt->bind_param("i", $lid);
         $stmt->execute();
         $result = $stmt->get_result();
