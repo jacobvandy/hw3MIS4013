ac0083d23777a5a1a4c1ade8fddd97713ed373f9
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-04 13:23:39 -0500

    Update view-resturant-with-employees.php

--- view-resturant-with-employees.php
+++ view-resturant-with-employees.php
@@ -4,7 +4,7 @@
     
  
       <?php
-while ($resturants = $restaurant->fetch_assoc()) {
+while ($restaurants = $restaurant->fetch_assoc()) {
   ?>
   <div class="card">
    
@@ -13,7 +13,7 @@ while ($resturants = $restaurant->fetch_assoc()) {
       <p class="card-text">
         <ul class="list-group">
         <?php
-        $menu = SelectMenuByRestaurant($resturants['RestaurantID'];);
+        $menu = SelectMenuByRestaurant($restaurants['RestaurantID'];);
         while ($menus = $menu->fetch_assoc()) {
           ?>
           <li class="list-group-item"><?php echo $menus['ItemName']; ?> - <?php echo $menus['Price']; ?> - <?php echo $menus['State']; ?></li>
@@ -24,7 +24,7 @@ while ($resturants = $restaurant->fetch_assoc()) {
         ?>
           </ul>
         </p>
-      <p class="card-text"><small class="text-body-secondary">TacoBell Rating: <?php echo $resturants['Rating']; ?></small></p>
+      <p class="card-text"><small class="text-body-secondary">TacoBell Rating: <?php echo $restaurants['Rating']; ?></small></p>
     </div>
   </div>
 
