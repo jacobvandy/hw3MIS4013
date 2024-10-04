e752005fd566ccc4a8d8d8a7bc0ef6873d37e6bf
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-03 19:35:22 -0500

    Create locations-by-menu.php

---
+++ locations-by-menu.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-locations-by-menu.php");
+
+$pageTitle = "Menu Items By Restaurant";
+include "view-header.php";
+$menu = SelectLocationsByRestaurantMenu($_POST['lid']);
+include "view-locations-by-menu.php";
+include "view-footer.php";
+?>
