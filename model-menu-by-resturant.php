9fb89e53e0cf00a3d13ab05712cdc1cd97245647
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-07 10:45:41 -0500

    Update model-menu-by-resturant.php

--- model-menu-by-resturant.php
+++ model-menu-by-resturant.php
@@ -2,7 +2,7 @@
 function SelectMenuByRestaurant($iid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT mi.MenuItemID, ItemName, Price, City, State, Address FROM menuitems mi join locations l on mi.LocationID = l.LocationID WHERE l.RestaurantID = ?");
+        $stmt = $conn->prepare("SELECT mi.MenuItemID, ItemName, Price, City, State, Address FROM menuitems mi join locations l on mi.LocationID = l.LocationID WHERE mi.RestaurantID = ?");
         $stmt->bind_param("i", $iid);
         $stmt->execute();
         $result = $stmt->get_result();
