53ef53adef9d70c3a668c0ef575dc35b68ad4e5d
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-21 11:37:05 -0500

    Update employees-by-location.php

--- employees-by-location.php
+++ employees-by-location.php
@@ -1,10 +1,12 @@
+
+
 <?php
 require_once("util-db.php");
 require_once("model-employees-by-location.php");
 
 $pageTitle = "Menu Items By Restaurant";
 include "view-header.php";
-$emp = SelectEmpByLocation($_GET['id']);
+$emp = SelectEmpByLocation($_POST['lid']);
 include "view-employees-by-location.php";
 include "view-footer.php";
 ?>
