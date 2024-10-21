a74db04e58378b1a253d4500c16f4dd60e76e1fb
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-21 11:38:32 -0500

    Create view-employees-by-location.php

---
+++ view-employees-by-location.php
@@ -0,0 +1,36 @@
+<h1>Menu by Location</h1>
+<div class="table-responsive">
+  <table class="table">
+  <thead>
+    <tr>
+      <th>ItemID</th>
+    <th>ItemName</th>
+    <th>Price</th>
+    <th>City</th>
+    <th>State</th>
+    <th>Address</th>
+      
+
+    </tr>
+    
+  </thead>
+    <tbody>
+      <?php
+while ($location = $locations->fetch_assoc()) {
+  ?>
+<tr>
+  <td><?php echo $location['LocationID']; ?></td>
+  <td><?php echo $location['RestaurantID']; ?></td>
+  <td><?php echo $location['Price']; ?></td>
+  <td><?php echo $location['City']; ?></td>
+  <td><?php echo $location['State']; ?></td>
+  <td><?php echo $location['Address']; ?></td>
+
+</tr>
+      <?php
+}
+?>
+    </tbody>
+
+  </table>
+</div>
