4c4453d17a5f2cff798c3f163fe69f2351bcb5a6
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 13:45:49 -0500

    Update model-menu.php

--- model-menu.php
+++ model-menu.php
@@ -12,4 +12,46 @@ function SelectMenu() {
         throw $e;
     }
 }
+
+function insertMenu($miName, $rPrice) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO menuitems (ItemName, Price) VALUES (?, ?)");
+        $stmt->bind_param("sd", $miName, $rPrice);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateMenu($miName, $miPrice, $mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE menuitems SET ItemName = ?, Price = ? WHERE MenuItemID = ?");
+        $stmt->bind_param("sdi", $miName, $miPrice, $mid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletemenu($mid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM menuitems WHERE MenuItemID = ?");
+        $stmt->bind_param("i", $mid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
