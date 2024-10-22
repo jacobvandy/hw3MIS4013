d361e77838167daa3c4e97ce487cda6133206d99
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 13:57:44 -0500

    Update view-menu.php

--- view-menu.php
+++ view-menu.php
@@ -1,4 +1,14 @@
-<h1>Taco Bells</h1>
+<div class= "row">
+  <div class = "col">
+    <h1>Taco Bells</h1>
+  </div>
+  <div class = "col-auto">
+
+    <?php
+include "view-menu-newform.php";
+?>
+</div>
+</div>
 <div class="table-responsive">
   <table class="table">
   <thead>
@@ -6,6 +16,9 @@
       <th>ItemID</th>
     <th>ItemName</th>
     <th>Price</th>
+       <th></th>
+      <th></th>
+      <th></th>
       <th></th>
 
     </tr>
@@ -19,6 +32,22 @@ while ($menus = $menu->fetch_assoc()) {
   <td><?php echo $menus['MenuItemID']; ?></td>
   <td><?php echo $menus['ItemName']; ?></td>
   <td><?php echo $menus['Price']; ?></td>
+  <td>
+    <form method="post" action="">
+  <input type="hidden" name="mid" value="<?php echo $menus['MenuItemID']; ?>">
+  <input type="hidden" name="actionType" value="Delete">
+  <button type="submit" class="btn btn-primary" onclick="return confirm('Confirm Changes?');">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+</svg>
+  </button>
+</form>
+  </td>
+  <td>
+     <?php
+include "view-menu-editform.php";
+?>
+  </td>
   <td> 
 <form method="post" action="locations-by-menu.php">
   <input type="hidden" name="lid" value="<?php echo $menus['MenuItemID']; ?>">
