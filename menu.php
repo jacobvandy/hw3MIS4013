7a191f9776187489d6dc0560eb242f93bb4717b2
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 13:45:04 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -4,6 +4,37 @@ require_once("model-menu.php");
 
 $pageTitle = "Taco Bell Menu Items";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+ switch ($_POST['actionType']) {
+   case "Add":
+   if (insertMenu($_POST['miName'], $_POST['miPrice'])) {
+    echo '<div class="alert alert-success" role="alert">Menu Item Added</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error adding Menu Item</div>';
+   }
+  
+   break;
+   case "Edit":
+   if (updateMenu($_POST['miName'], $_POST['miPrice'], $_POST['mid'])) {
+    echo '<div class="alert alert-success" role="alert">MenuItem Edited</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error editing Menu Item</div>';
+   }
+  
+   break;
+  case "Delete":
+   if (deleteMenu($_POST['mid'])) {
+    echo '<div class="alert alert-success" role="alert">Menu Item Deleted</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error deleting MenuItem</div>';
+   }
+  
+   break;
+ }
+}
+
+
 $menu = SelectMenu();
 include "view-menu.php";
 include "view-footer.php";
