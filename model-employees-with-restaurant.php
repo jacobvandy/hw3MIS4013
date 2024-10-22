81fbeba9d5ff75daed6e66c4ba5e2706dce5ce8b
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 16:44:01 -0500

    Update model-employees-with-restaurant.php

--- model-employees-with-restaurant.php
+++ model-employees-with-restaurant.php
@@ -47,5 +47,31 @@ function insertEmpLoc($elEmployeeID, $elEmployeeName, $elAddress, $elCity, $elSt
     }
 }
 
+function updateEmpLoc($elEmployeeID, $elEmployeeName, $elAddress, $elCity, $elState, $elZipCode, $locid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE locations SET EmployeeID = ?, EmployeeName = ?, Address = ?, City= ?, State= ?, ZipCode= ? WHERE LocationID = ?");
+        $stmt->bind_param("issssii", $elEmployeeID, $elEmployeeName, $elAddress, $elCity, $elState, $elZipCode, $locid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
+function deleteEmpLoc($locid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM locations WHERE LocationID = ?");
+        $stmt->bind_param("i", $locid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
