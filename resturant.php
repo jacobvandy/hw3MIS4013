ed8678c7dfbe3a72f5f7b71f1e28a73ceccdb966
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 11:58:12 -0500

    Update resturant.php

--- resturant.php
+++ resturant.php
@@ -4,6 +4,16 @@ require_once("model-resturant.php");
 
 $pageTitle = "Taco Bell";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+ switch ($_POST['actionType']) {
+   case "Add":
+   insertRestaurant($_POST['rName'], $_POST['rRating'], $_POST['rFoodType']);
+   break;
+ }
+}
+
+
 $restaurant = SelectRestaurant();
 include "view-resturant.php";
 include "view-footer.php";
