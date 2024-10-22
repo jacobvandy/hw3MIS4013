30c5292edc739c6afd2db57d921551540e863a03
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 15:42:43 -0500

    Update employees-with-restaurant.php

--- employees-with-restaurant.php
+++ employees-with-restaurant.php
@@ -4,6 +4,23 @@ require_once("model-employees-with-restaurant.php");
 
 $pageTitle = "Taco Bell and their Employees";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) {
+ switch ($_POST['actionType']) {
+   case "Add":
+   if (insertEmpLoc($_POST['elLocationID'], $_POST['elName'], $_POST['elAddress'], $_POST['elCity'], $_POST['elState'], $_POST['elZipCode'])) {
+    echo '<div class="alert alert-success" role="alert">Employee Added</div>';
+   } else {
+    '<div class="alert alert-danger" role="alert">Error adding Employee</div>';
+   }
+  
+   break;
+ 
+ }
+}
+
+
 $restaurant = SelectRestaurant();
 include "view-employees-with-restaurant.php";
 include "view-footer.php";
