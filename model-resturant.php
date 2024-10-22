9223cf46cd138b7edf9a5ac99e0d903a3ee45069
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 10:52:09 -0500

    Update model-resturant.php

--- model-resturant.php
+++ model-resturant.php
@@ -12,4 +12,47 @@ function SelectRestaurant() {
         throw $e;
     }
 }
+
+
+function insertRestaurant($rName, $rRating, $rFoodType) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO TacoBell (Name, Rating, FoodType) VALUES (?, ?, ?)");
+        $stmt->bind_param("sds", $rName, $rRating, $rFoodType);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateRestaurant($rName, $rRating, $rFoodType, $rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE TacoBell SET 'Name' = ?, 'Rating' = ?, 'FoodType' = ? WHERE RetaurantID = ?");
+        $stmt->bind_param("sdsi", $rName, $rRating, $rFoodType, $rid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteRestaurant($rid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM tacobell WHERE RestaurantID = ?");
+        $stmt->bind_param("i", $rid);
+        $success =  $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
