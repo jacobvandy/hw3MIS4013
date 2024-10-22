9ccba7867534aa17c49bfe4ac9d45c4c5ec844a0
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-22 16:07:25 -0500

    Update employees-with-restaurant.php

--- employees-with-restaurant.php
+++ employees-with-restaurant.php
@@ -9,7 +9,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
  switch ($_POST['actionType']) {
    case "Add":
-   if (insertEmpLoc($_POST['elAddress'], $_POST['elCity'], $_POST['elState'], $_POST['elZipCode'])) {
+   if (insertEmpLoc($_POST['elRestaurantID'], $_POST['elAddress'], $_POST['elCity'], $_POST['elState'], $_POST['elZipCode'])) {
     echo '<div class="alert alert-success" role="alert">Employee Added</div>';
    } else {
     '<div class="alert alert-danger" role="alert">Error adding Employee</div>';
