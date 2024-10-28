ef4e0737c620ce59650295323abc4c00bedfc56e
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-10-29 10:10:45 +1300

    Update view-employees-with-restaurant.php

--- view-employees-with-restaurant.php
+++ view-employees-with-restaurant.php
@@ -26,7 +26,7 @@ while ($restaurants = $restaurant->fetch_assoc()) {
         $eid = SelectEmpByLocation($restaurants['RestaurantID']);
         while ($eids = $eid->fetch_assoc()) {
           ?>
-          <li class="list-group-item"><?php echo $eids['EmployeeID']; ?> - <?php echo $eids['EmployeeName']; ?> - <?php echo $eids['Address']; ?> - <?php echo $eids['City']; ?> - <?php echo $eids['State']; ?> - <?php echo $eids['ZipCode'] - <?php echo $eids['LocationID']; ?> - <?php echo $eids['RestaurantID']; ?>
+          <li class="list-group-item"><?php echo $eids['EmployeeID']; ?> - <?php echo $eids['EmployeeName']; ?> - <?php echo $eids['Address']; ?> - <?php echo $eids['City']; ?> - <?php echo $eids['State']; ?> - <?php echo $eids['ZipCode']; ?>
          <form method="post" action="">
                 <input type="hidden" name="locid" value="<?php echo $eids['LocationID']; ?>">
                 <input type="hidden" name="actionType" value="Delete">
