5daae8e2987b7a9c572c12cf2408d0a41ca414fd
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-06 19:34:06 -0600

    Update model-employees-with-restaurant.php

--- model-employees-with-restaurant.php
+++ model-employees-with-restaurant.php
@@ -35,7 +35,7 @@ WHERE e.LocationID = ?");
 function SelectRestaurantForInput() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT EmployeeID, EmployeeName FROM employeees ORDER BY EmployeeName");
+        $stmt = $conn->prepare("SELECT EmployeeID, EmployeeName FROM employees ORDER BY EmployeeName");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
