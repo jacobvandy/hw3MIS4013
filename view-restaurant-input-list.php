10e6a42fcd91d38aef9609c84540e5593ef5092d
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-06 19:27:18 -0600

    Create view-restaurant-input-list.php

---
+++ view-restaurant-input-list.php
@@ -0,0 +1,10 @@
+<select class="form-select" id = "elRID" name = "elRID">
+<?php 
+while ($restaurantItem = $restaurantList->fetch_assoc()) {
+?>
+   <option value="<?php echo ($restaurantItem['RestaurantID']; ?>" ><?php echo ($restaurantItem['Name']; ?></option>
+<?php  
+}
+?>
+
+</select>
