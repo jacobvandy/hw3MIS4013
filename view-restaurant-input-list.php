b3f4486ca1babcfdcbb1b820d3c388492927ed71
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-06 20:14:54 -0600

    Update view-restaurant-input-list.php

--- view-restaurant-input-list.php
+++ view-restaurant-input-list.php
@@ -1,8 +1,12 @@
 <select class="form-select" id = "elRID" name = "elRID">
 <?php 
 while ($restaurantItem = $restaurantList->fetch_assoc()) {
+$selText = "";
+if ($selectedEmployee == $restaurantItem['RestaurantID'];) {
+   $selText = "selected";
+}
 ?>
-   <option value="<?php echo $restaurantItem['RestaurantID']; ?>" ><?php echo $restaurantItem['Name']; ?></option>
+   <option value="<?php echo $restaurantItem['RestaurantID']; ?>" <?=$selText?> ><?php echo $restaurantItem['Name']; ?></option>
 <?php  
 }
 ?>
