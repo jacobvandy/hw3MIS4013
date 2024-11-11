79cc26d7c7ef098a964298cd8caa2f898658af66
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-11 17:59:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ include "view-header.php";
     <h1>HW4 MIS4013</h1>
 <img id="tbimage" src="TBdecades.jpg" style="height:200px;"/>
 
-<button id="addbtn"class="btn btn-primary"><i class="bi bi-file-earmark-plus"></i></button>
+<button id="plusbtn"class="btn btn-primary"><i class="bi bi-plus-circle-fill"></i></button>
     <button  id="minusbtn"class="btn btn-danger">
     <i class="bi bi-dash-circle-fill"></i>
     </button>
     <script>
         "use strict";
-        document.querySelector("#addbtn").addEventListener("click", () =>
+        document.querySelector("#plusbtn").addEventListener("click", () =>
         {
             let w=document.querySelector("#tbimage").width;
             w = w + 10;
