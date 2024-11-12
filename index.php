a1ce7c37f7d207b5a7b4314df79c82e409b5e006
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-11 18:29:11 -0600

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,6 @@ include "view-header.php";
 <img id="tbimage" src="TBdecades.jpg" style="height:200px;"/>
 
 
-
-
-
-
-
-
-
-
-
-
-
 <button id="plusbtn"class="btn btn-primary">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"/>
@@ -43,6 +32,10 @@ include "view-header.php";
         }
         );
     </script>
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
 <?php
 include "view-footer.php";
 ?>
