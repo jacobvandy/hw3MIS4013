92e1b6534126257f4c266508e3e55568a230da1d
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-18 08:17:52 +1100

    Create view-menuprice-chart.php

---
+++ view-menuprice-chart.php
@@ -0,0 +1,49 @@
+<h1>Menu and their Prices</h1>
+
+
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+
+     
+
+
+<script>
+ var options = {
+          series: [{
+          data: [
+<?php
+while ($menus = $menu->fetch_assoc()) {
+    echo $menus['Price'];
+}
+  ?> 
+]
+        }],
+          chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            borderRadius: 4,
+            borderRadiusApplication: 'end',
+            horizontal: true,
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [<?php
+while ($menus = $menu->fetch_assoc()) {
+    echo $menus['ItemName'];
+}
+  ?> ],
+        }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+</script>
+
+
+
