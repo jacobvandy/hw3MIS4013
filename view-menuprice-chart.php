d7682918946451cd0c9713d6468e98cde5bd394f
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-18 09:20:04 +1100

    Update view-menuprice-chart.php

--- view-menuprice-chart.php
+++ view-menuprice-chart.php
@@ -7,16 +7,24 @@
  <h1>Menu and their Prices</h1>
   
 
+<?php
+
+$menu = SelectMenu();
+$prices = [];
+$itemNames = [];
+while ($menus = $menu->fetch_assoc()) {
+    $prices[] = $menus['Price'];
+    $itemNames[] = addslashes($menus['ItemName']);
+}
 
 
+echo "
 <script>
-
-
-   
+    // ECharts instance initialization
     const chartDom = document.getElementById('menuChart');
     const myChart = echarts.init(chartDom);
 
-
+    // Chart configuration
     const option = {
         title: {
             text: 'Menu Prices',
@@ -28,13 +36,7 @@
         },
         xAxis: {
             type: 'category',
-            data: <?php
-$menu = SelectMenu();
-
-while ($menus = $menu->fetch_assoc()) {
-    echo $menus['ItemName'];
-}
-?>
+            data: " . json_encode($itemNames) . "
         },
         yAxis: {
             type: 'value',
@@ -43,19 +45,14 @@ while ($menus = $menu->fetch_assoc()) {
         series: [{
             name: 'Price',
             type: 'bar',
-            data: <?php
-$menu = SelectMenu();
-
-while ($menus = $menu->fetch_assoc()) {
-    echo $menus['price'];
-}
-?>,
+            data: " . json_encode($prices) . ",
             itemStyle: {
                 color: '#5470C6'
             }
         }]
     };
 
-
+    // Render the chart
     myChart.setOption(option);
 </script>
+";
