3fde33144fecadbec76bf41c094f7b27c8d41cb1
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-18 13:27:16 -0600

    Update view-menu-editform.php

--- view-menu-editform.php
+++ view-menu-editform.php
@@ -23,7 +23,7 @@
         </div>
         <div class="mb-3">
           <label for="miPrice<?php echo $menus['MenuItemID']; ?>" class="form-label">Price  </label>
-          <input type="text" class="form-control" id="miPrice<?php echo $menus['MenuItemID']; ?>" name="miPrice" value="<?php $menus['Price']; ?>">
+          <input type="text" class="form-control" id="miPrice<?php echo $menus['MenuItemID']; ?>" name="miPrice" value="<?php echo $menus['Price']; ?>">
         </div>  
       
         <input type="hidden" name="mid"value="<?php echo $menus['MenuItemID']; ?>">
