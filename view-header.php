7f03f5f0b93eb4e6e5166cf7e5712255587ac130
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-20 09:42:44 +1100

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -40,6 +40,9 @@
         <li class="nav-item">
           <a class="nav-link" href="menu-chart.php">Menu Chart</a>
         </li>
+          <li class="nav-item">
+          <a class="nav-link" href="price-sum.php">Price Summary</a>
+        </li>
       </ul>
       
     </div>
