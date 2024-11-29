a53056cb2b3cea3fae7469a78a1cd2482bbdf083
jacobvandy <123128135+jacobvandy@users.noreply.github.com>
2024-11-29 11:15:14 -0600

    Update price-sum.php

--- price-sum.php
+++ price-sum.php
@@ -5,7 +5,7 @@ require_once("model-price-sum.php");
 $pageTitle = "Price Summary";
 include "view-header.php";
 
-$menu1 = SelectMenus();
+$candy1 = SelectCandys();
 include "view-price-sum.php";
 include "view-footer.php";
 ?>
